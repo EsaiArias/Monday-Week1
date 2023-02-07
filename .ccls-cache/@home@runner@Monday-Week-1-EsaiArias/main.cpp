934c40cab920dad11110d5eeb60fee961194c03a
EsaiArias <100735475+EsaiArias@users.noreply.github.com>
2023-02-07 04:47:01 +0000

    created code

--- .ccls-cache/@home@runner@Monday-Week-1-EsaiArias/main.cpp
+++ .ccls-cache/@home@runner@Monday-Week-1-EsaiArias/main.cpp
@@ -1,18 +1,25 @@
 // Monday-Week-1
 // Due: Friday, February 10, 11:59 PM
 /*************************************************
-Student First Name: 
-Student Last Name: 
+Student First Name: Esai 
+Student Last Name: Arias
 **************************************************/
 
 #include <iostream>
 using namespace std;
 
 int main() {
-  cout << "Hello World!\n";
+  string Name;
+  
+  cout << "Hello CS-216!\n"
+          "Enter your name: ";
+  getline(cin, Name);
+  cout << "Welcome " << Name;
 }
 
 /*********************************************
 // Output:
-
+Hello CS-216!
+Enter your name: Esai Arias
+Welcome Esai Arias
 **********************************************/
